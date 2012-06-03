764ddd061d1855bff0819d13443fe1e023c22da5
david.ftzi <david.ftzi@368dd8da-9d08-7edb-46ac-11dfb8087b4a>
2012-06-03 18:05:42 +0000

    carcass

---
+++ fishapp/src/com/fishapp/service/ParsingService.java
@@ -0,0 +1,16 @@
+package com.fishapp.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ParsingService extends IntentService {
+
+	public ParsingService(final String name) {
+		super(name);
+	}
+
+	@Override
+	protected void onHandleIntent(final Intent intent) {
+	}
+
+}
