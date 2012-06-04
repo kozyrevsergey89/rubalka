bafc5deec2cca95bd6408f1559d119e34cf7c950
kozyrevsergey89@gmail.com <kozyrevsergey89@gmail.com@368dd8da-9d08-7edb-46ac-11dfb8087b4a>
2012-06-04 01:31:27 +0000

    first mega-commit

--- trunk/fishapp/src/com/fishapp/FishappActivity.java
+++ trunk/fishapp/src/com/fishapp/FishappActivity.java
@@ -1,13 +1,45 @@
 package com.fishapp;
 
+import com.fishapp.service.ParsingService;
+
 import android.app.Activity;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.AbsListView.RecyclerListener;
 
 public class FishappActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	public String temperature;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //starting intent-service
+        startService(new Intent(this, ParsingService.class));
+        
+        BroadcastReceiver sentSmsBroadcastCome = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //Toast.makeText(context, "SMS SENT!!", Toast.LENGTH_SHORT).show();
+            	if (intent.getExtras() != null){
+            		temperature = intent.getStringExtra("weather"); 
+            	}
+            }
+        };
+        IntentFilter filterSend = new IntentFilter();
+        filterSend.addAction("toActivity");
+        registerReceiver(sentSmsBroadcastCome, filterSend);
+        
     }
+    
+    
+    
+    
 }
--- trunk/fishapp/src/com/fishapp/service/ParsingService.java
+++ trunk/fishapp/src/com/fishapp/service/ParsingService.java
@@ -1,5 +1,11 @@
 package com.fishapp.service;
 
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.fishapp.service.parser.XmlParser;
+
 import android.app.IntentService;
 import android.content.Intent;
 
@@ -8,9 +14,25 @@ public class ParsingService extends IntentService {
 	public ParsingService(final String name) {
 		super(name);
 	}
+	
+	public ParsingService(){
+		super("parsingService");
+	} 
 
 	@Override
 	protected void onHandleIntent(final Intent intent) {
+		XmlParser parser = new XmlParser(this);
+		try {
+			parser.parseXml("http://informer.gismeteo.ru/xml/33345_1.xml");
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
