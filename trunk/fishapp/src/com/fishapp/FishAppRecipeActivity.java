de83d2f136247d76d848d7ad652a3e9b4e7da6a1
david.ftzi <david.ftzi@368dd8da-9d08-7edb-46ac-11dfb8087b4a>
2012-06-24 02:22:10 +0000

    updated some features

--- trunk/fishapp/src/com/fishapp/FishAppRecipeActivity.java
+++ trunk/fishapp/src/com/fishapp/FishAppRecipeActivity.java
@@ -6,15 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FishAppRecipeActivity extends Activity {
     /** Called when the activity is first created. */
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-		
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fish_app_recipe_layout);
 		
+		TextView logo = (TextView) findViewById(R.id.action_bar_logo);
+		logo.setText(getString(R.string.cook_book));
 		final Button button1 = (Button) findViewById(R.id.recipebutton1);
 		final Button button2 = (Button) findViewById(R.id.recipebutton2);
 		final Button button3 = (Button) findViewById(R.id.recipebutton3);
