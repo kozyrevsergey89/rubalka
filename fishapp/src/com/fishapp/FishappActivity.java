99b8cc974b54b4153765b6c083b5c9b93227dee4
david.ftzi <david.ftzi@368dd8da-9d08-7edb-46ac-11dfb8087b4a>
2012-06-29 13:36:58 +0000

    parser fixed. fixed prefedittor.commit();!!!!!!!!!!!!!!!!!

--- fishapp/src/com/fishapp/FishappActivity.java
+++ fishapp/src/com/fishapp/FishappActivity.java
@@ -2,9 +2,7 @@ package com.fishapp;
 
 import java.util.Calendar;
 import java.util.Random;
-
 import com.fishapp.service.ParsingService;
-
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -16,14 +14,15 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class FishappActivity extends Activity {
     /** Called when the activity is first created. */
-	public String temperature="+19";
-	public String cloudiness="0";
-	public String presipatation="0";
+	public String tempMax="0";
+	public String tempMin="0";
+	public int cloudiness=0;
+	public int presipatation=0;
 	private BroadcastReceiver sentSmsBroadcastCome;
+	private WhetherReceiver whetherReceiver;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,34 +31,33 @@ public class FishappActivity extends Activity {
         //starting intent-service
         TextView actionBaView = (TextView) findViewById(R.id.action_bar_logo);
         actionBaView.setText(getString(R.string.weather));
-        sentSmsBroadcastCome = new BroadcastReceiver() {
-
-            @Override
-            public void onReceive(Context context, Intent intent) {
-            	if ((intent.getExtras() != null)&&(intent.getStringExtra("weatherJob")=="done")){
-            		getFromSharedPreferences();
-            		displayWeather();
-            	}
-            }
-        };
-        
     }
 	
 	@Override
 	protected void onStart() {
 		super.onStart();
+		whetherReceiver = new WhetherReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("toActivity");
+		registerReceiver(whetherReceiver, intentFilter);
         if (getConnInfo()){
         	//starting service with xml parsing
         	startService(new Intent(this, ParsingService.class));
-            //registering receiver
-        	IntentFilter filterSend = new IntentFilter();
-            filterSend.addAction("toActivity");
-            registerReceiver(sentSmsBroadcastCome, filterSend);
+        	
+            
         }	
         getFromSharedPreferences();
         displayWeather();
 
 	}
+	
+	private class WhetherReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context arg0, Intent intent) {
+			// TODO Auto-generated method stub
+			getFromSharedPreferences();
+	        displayWeather();
+		}}
 	
 	@Override
 	protected void onStop() {
@@ -67,7 +65,7 @@ public class FishappActivity extends Activity {
 		super.onStop();
 	}
 	
-	private void displayWeather(){
+	public void displayWeather(){
 		ImageView bigWeather = (ImageView) findViewById(R.id.bigweather);
 		ImageView bigMoon = (ImageView) findViewById(R.id.bigmoon);
 		ImageView smallWeather1 = (ImageView) findViewById(R.id.smallweather1);
@@ -75,14 +73,12 @@ public class FishappActivity extends Activity {
 		ImageView smallWeather3 = (ImageView) findViewById(R.id.smallweather3);
 		ImageView smallWeather4 = (ImageView) findViewById(R.id.smallweather4);
 		TextView weatherInfo = (TextView) findViewById(R.id.weathertext);
-		if ((temperature.length()>0)&&(temperature.length() > 0)){
-			weatherInfo.setText(temperature);
-		}
-		if ((cloudiness.length()>0)&&(Integer.parseInt(cloudiness)) > 0){
+		weatherInfo.setText("Температура сегодня \n"+"от "+tempMin+" до "+tempMax);
+		if (cloudiness > 0){
 			bigWeather.setImageLevel(2);
 			smallWeather1.setImageLevel(2);
 		}
-		if ((presipatation.length()>0)&&(Integer.parseInt(presipatation)>0)){
+		if (presipatation>0){
 			bigWeather.setImageLevel(3);
 			smallWeather1.setImageLevel(3);
 		}
@@ -101,19 +97,16 @@ public class FishappActivity extends Activity {
 		}else{
 			bigMoon.setImageLevel(3);
 		}
-		
-		
-		
 	}
 	
 	private void getFromSharedPreferences(){
-		
 		SharedPreferences prefs = getSharedPreferences("wheaterPreference", MODE_PRIVATE);
 		if (!prefs.getAll().isEmpty()){
-			Toast.makeText(this, "preferences inside!!!", 1000).show();
-			temperature = prefs.getString("TemperatureMax", "not found");
-			cloudiness = prefs.getString("cloudiness", "not found");
-			presipatation = prefs.getString("presipatation", "not found");
+			tempMax = prefs.getString("MAX", "not found");
+			tempMin = prefs.getString("MIN", "not found");
+			int def = 0;
+			cloudiness = prefs.getInt("cloudiness", def);
+			presipatation = prefs.getInt("presipatation", def);
 			
 		}
 		
@@ -122,7 +115,6 @@ public class FishappActivity extends Activity {
 	
 	public Boolean getConnInfo(){
         ConnectivityManager conMgr =  (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
 		NetworkInfo i = conMgr.getActiveNetworkInfo();
 		  if (i == null)
 		    return false;
@@ -131,9 +123,6 @@ public class FishappActivity extends Activity {
 		  if (!i.isAvailable())
 		    return false;
 		  return true;
-		
 	}
-	
-	
     
 }
--- fishapp/src/com/fishapp/service/parser/XmlParser.java
+++ fishapp/src/com/fishapp/service/parser/XmlParser.java
@@ -1,20 +1,22 @@
 package com.fishapp.service.parser;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-
-import org.xmlpull.v1.XmlPullParser;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
 import com.fishapp.FishappActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class XmlParser {
 	
@@ -27,46 +29,74 @@ public class XmlParser {
 	
 	public void parseXml(String weatherUrl) throws XmlPullParserException, IOException
 	{
-			  XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		      XmlPullParser parser = factory.newPullParser();
-
-		      InputStream in;
-		      try {
-		        URL u = new URL(weatherUrl);
-		        in = u.openStream();
-		      }
-		      catch (MalformedURLException ex) {
-		          // Maybe it's a file name :)
-		          in = new FileInputStream(weatherUrl);
-		      }
-		      parser.setInput(in, null);
-		//preparing to write data to shared preferences
+		String min = null;
+	    String max = null;
+	    String pheno = null;
+	    String cloud = null;
+	    int cloudiness=0;
+	    int precipitation=0;
+		
+	    //preparing to write data to shared preferences
 	    SharedPreferences settings = context.getSharedPreferences("wheaterPreference", Context.MODE_PRIVATE);
 	    SharedPreferences.Editor prefEditor = settings.edit();
-	    //use preferences to prefEditor.putString("TemperatureMax", temperatureMax);
-
-	    while (!"FORECAST".equals(parser.getName()) && parser.getEventType() != XmlPullParser.START_TAG) {
-	        parser.next();
-	    }
-	    //writing date to pref
-	    prefEditor.putString("forecastDay", parser.getAttributeValue(null, "day"));
-	    prefEditor.putString("forecastMonth", parser.getAttributeValue(null, "month"));
-	    while (!"PHENOMENA".equals(parser.getName()) && parser.getEventType() != XmlPullParser.START_TAG) {
-	        parser.next();
-	    }
-	    prefEditor.putString("cloudiness", parser.getAttributeValue(null, "cloudiness"));
-	    prefEditor.putString("precipitation", parser.getAttributeValue(null, "precipitation"));
-	    while (!"TEMPERATURE".equals(parser.getName()) && parser.getEventType() != XmlPullParser.START_TAG) {
-	        parser.next();
-	    }
-	    prefEditor.putString("temperaturemax", parser.getAttributeValue(null, "max"));
-	    prefEditor.putString("temperaturemin", parser.getAttributeValue(null, "min"));
 	    
+	    try {
+
+	        final URL currentUrl = new URL(weatherUrl);
+
+	        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	        final DocumentBuilder builder = factory.newDocumentBuilder();
+
+	        final Document doc = builder.parse(new InputSource(currentUrl.openStream()));
+	        doc.getDocumentElement().normalize();
+
+	        final NodeList nodeList = doc.getElementsByTagName("TEMPERATURE");
+	        final Node node = nodeList.item(2);
+
+	        final NodeList phenoList = doc.getElementsByTagName("PHENOMENA");
+	        final Node pheNode = phenoList.item(2);
+
+	        final NamedNodeMap attribute = node.getAttributes();
+	        final Node value = attribute.getNamedItem("min");
+
+	        final NamedNodeMap map = pheNode.getAttributes();
+	        final Node mapVal = map.getNamedItem("cloudiness");
+
+	        min = value.getNodeValue();
+	        cloud = mapVal.getNodeValue();
+
+	        final NamedNodeMap maxAttr = node.getAttributes();
+	        final Node maxValue = maxAttr.getNamedItem("max");
+
+	        final NamedNodeMap pre = pheNode.getAttributes();
+	        final Node preVal = pre.getNamedItem("precipitation");
+
+	        max = maxValue.getNodeValue();
+	        pheno = preVal.getNodeValue();
+
+	        cloudiness = Integer.parseInt(cloud);
+	        precipitation = Integer.parseInt(pheno);
+	        
+	      } catch (final ParserConfigurationException e) {
+	        Log.e("XMLParser", "parser exception", e);
+	      } catch (final SAXException e) {
+	        Log.e("XMLParser", "SAXException", e);
+	      } catch (final IOException e) {
+	        Log.e("XMLParser", "IOException", e);
+	      }
+	    
+	    //putting data to shared prefs
+	    
+	    prefEditor.putString("MAX", max);
+	    prefEditor.putString("MIN", min);
+	    prefEditor.putInt("cloudiness", cloudiness);
+	    prefEditor.putInt("precipitation", precipitation);
+	    prefEditor.commit();
 	    
 	    
 	    //sending intent extras back to activity
 	    Intent toActivity = new Intent(context, FishappActivity.class);
-	    toActivity.putExtra("weatherJob", "done" );
+	    toActivity.setAction("toActivity");
 	    context.sendBroadcast(toActivity);
 		}
 
